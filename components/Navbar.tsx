514752ba4d3311f2110f00ac6eccfee2e0f818ff
Rudra Singh Chauhan <rudra.dev.builds@gmail.com>
2026-04-10 13:14:01 +0530

    feat: Optimize image loading in Navbar
    Adds the `priority` prop to the logo image in the Navbar component. This will help improve perceived loading performance by telling the browser to fetch this image with higher priority.

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -30,6 +30,7 @@ export default function Navbar() {
                 width={100}
                 height={40}
                 className="object-contain hover:opacity-80 transition-opacity"
+                priority
               />
             </Link>
           </div>
